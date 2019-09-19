b7d310aaab539ee48f2fb6d3e02819b99c1af5b8
Kris Bernard Yango <krisbernardyango@Kriss-MacBook-Pro.local>
2019-09-19 16:18:13 +0800

    setup

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -162,6 +162,12 @@
         />
       </a-col>
     </a-row>
+    <div id="fb-root"></div>
+   <div class="fb-customerchat"
+      attribution=setup_tool
+      page_id="115862123135923"
+      theme_color="#fa3c4c">
+    </div>
   </div>
 </template>
 
@@ -191,24 +197,52 @@ export default {
     }
   }
 };
-window.fbAsyncInit = function() {
-  FB.init({
-    appId: "314648529334246",
-    xfbml: true,
-    version: "v4.0"
-  });
-};
+// window.fbAsyncInit = function() {
+//   FB.init({
+//     appId: "314648529334246",
+//     xfbml: true,
+//     version: "v4.0"
+//   });
+// };
 
-(function(d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
+// (function(d, s, id) {
+//   var js,
+//     fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s);
+//   js.id = id;
+//   js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+//   fjs.parentNode.insertBefore(js, fjs);
+// })(document, "script", "facebook-jssdk");
 </script>
+<script>
+// Facebook SDK plugin
+      (function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk");
+    </script>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId: "2463482707080808",
+          xfbml            : true,
+          version          : 'v4.0'
+        });
+      };
+
+      (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 
 <style>
 .textShadow {
