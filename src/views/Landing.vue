be0b2be266c78f7e896e430da6313eaadfed1fb2
Kris Bernard Yango <krisbernardyango@Kriss-MacBook-Pro.local>
2019-09-19 17:01:57 +0800

    dolores

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -166,8 +166,8 @@
    <div class="fb-customerchat"
         attribution=setup_tool
         page_id="115862123135923"
-      theme_color="#67b868">
-  </div>
+        theme_color="#67b868">
+      </div>
 </template>
 
 <script>
@@ -226,22 +226,26 @@ export default {
         fjs.parentNode.insertBefore(js, fjs);
       })(document, "script", "facebook-jssdk");
     </script>
-    <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId: "2463482707080808",
-          xfbml            : true,
-          version          : 'v4.0'
-        });
-      };
+    <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId: "2463482707080808",
+            xfbml            : true,
+            version          : 'v4.0'
+          });
+        };
 
-      (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+    
 
 <style>
 .textShadow {
